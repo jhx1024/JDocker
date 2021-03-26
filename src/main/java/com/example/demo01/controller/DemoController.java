5231472edd97810f43f6db4071545a9ebbb31a34
jhx1024 <jhx13145200@163.com>
2021-03-26 17:59:28 +0800

    Initial commit

---
+++ src/main/java/com/example/demo01/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.demo01.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/demo")
+    public String demo()
+    {
+        return "000";
+    }
+
+}
